1788d7b27f840951828baaea293ba9c537a989ac
zhoupeng <287214764@qq.com>
2021-07-31 11:36:58 +0800

    创建临时顺序节点

--- src/main/java/com/zp/MyCurator.java
+++ src/main/java/com/zp/MyCurator.java
@@ -5,6 +5,7 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.AddWatchMode;
+import org.apache.zookeeper.CreateMode;
 
 import java.util.List;
 
@@ -23,5 +24,8 @@ public class MyCurator {
         client.getData().forPath("/my/path");
         List<String> strings = client.getChildren().forPath("/my/path");
         Void aVoid = client.watchers().add().withMode(AddWatchMode.PERSISTENT).forPath("/my/path");
+
+
+        client.create().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath("/sdfsd");
     }
 }
