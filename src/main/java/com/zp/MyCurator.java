3502ff6d60dfdb1f6bd7731b1fdc61306c43b990
zhoupeng <287214764@qq.com>
2021-07-31 11:44:30 +0800

    启动时监听

--- src/main/java/com/zp/MyCurator.java
+++ src/main/java/com/zp/MyCurator.java
@@ -3,6 +3,8 @@ package com.zp;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.CuratorEvent;
+import org.apache.curator.framework.api.CuratorListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.AddWatchMode;
 import org.apache.zookeeper.CreateMode;
@@ -18,6 +20,12 @@ public class MyCurator {
         String zookeeperConnectionString = "";
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
         CuratorFramework client = CuratorFrameworkFactory.newClient(zookeeperConnectionString, retryPolicy);
+        client.getCuratorListenable().addListener(new CuratorListener() {
+            @Override
+            public void eventReceived(CuratorFramework client, CuratorEvent event) throws Exception {
+                System.out.println("event received");
+            }
+        });
         client.start();
         client.create().forPath("/my/path", "fsda".getBytes());
         client.setData().forPath("/my/path");
